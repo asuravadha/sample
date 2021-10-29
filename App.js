63514ca8346efcc9d4bcdd3a9778e265077eaaf9
asuravadha <rr.easwar@gmail.com>
2021-10-29 12:33:26 +0530

    init

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState, React } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [text] = useState(
+    "seaaaaaaaaaaaaaaaaaaa,aaaa,aaaaaaaaaaaaaaaaa.aaaaaaaaaaaaabbbb.bbbbbbbbbbbbb.bbbbbbbbbbbbbbbbbb.bbbbbbbbbbbbbbbbccccccccccccccc.ccccccccccccccccccccc.ccccccccccccccccccc"
+  );
+  const [showFull, setShowFull] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {showFull ? (
+        <Text style={styles.textToggle}>
+          {text}
+
+          <Text style={styles.textToggle} onPress={() => setShowFull(false)}>
+            {"less"}
+          </Text>
+        </Text>
+      ) : (
+        <Text style={styles.textToggle}>
+          {text.split(".")[0] + "."}
+
+          <Text style={styles.textToggle} onPress={() => setShowFull(true)}>
+            {"More"}
+          </Text>
+        </Text>
+      )}
     </View>
   );
 }
@@ -14,8 +33,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontWeight: "350",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textToggle: {
+    color: "#3742ff",
+    fontWeight: "350",
   },
 });
